2ac12c1dd08d712ddcbaade99b6fe1ce8919faf7
Sgray <phongnguyen6120@gmail.com>
2022-01-14 02:35:58 +0700

    fix loop in disappear

--- Assets/Scripts/Enermy/Behavior/ActionBehavior/NormalActionBehavior.cs
+++ Assets/Scripts/Enermy/Behavior/ActionBehavior/NormalActionBehavior.cs
@@ -14,7 +14,7 @@ namespace Assets.Scripts.Enermy.Behavior.ActionBehavior
             this.distanceView = distanceView;
             this.speed = speed;
         }
-        public void BehaveInContext(int direction, ref float speed, Rigidbody2D rigidbody2D, ref Animator animator, Action attack, Action<bool> moving)
+        public void BehaveInContext(int direction, ref float speed, ref Rigidbody2D rigidbody2D, ref Animator animator, Action attack, Action<bool> moving)
         {
             RaycastHit2D detectedPayer = Physics2D.Raycast(rigidbody2D.position + Vector2.up * 0.1f, new Vector2(direction, 0), distanceView, LayerMask.GetMask("Player"));
             if (detectedPayer.collider != null)
