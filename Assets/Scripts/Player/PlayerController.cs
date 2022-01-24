2393802ac5423bbe9553bcdf4f01fe3b9c365879
Phu Do <dophu1402@gmail.com>
2022-01-24 11:10:09 +0700

    new input system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     // rigibody prob
     private Rigidbody2D rb2d;
-    float horizontal;
-    float vertical;
 
     //Timer for damage
     public float timeInvincible = 0.6f;
@@ -37,7 +36,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 2.5f;
 
     //look direction
-    Vector2 lookDirection = new Vector2(0, 0);
+    Vector2 lookDirection = new Vector2(0, -1);
     //Animation
     private Animator animator;
 
@@ -61,23 +60,31 @@ public class PlayerController : MonoBehaviour
     // teleport
     private bool isAllowTeleport = true;
 
+    //input actions
+    PlayerInputActions inputActions;
+    Vector2 currentInput;
+
     // Start is called before the first frame update
     void Start()
     {
         checkPoint = transform.position;
         rb2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHearts = 3;
+        currentHearts = maxHearts;
         animator = GetComponent<Animator>();
         HeartSystem.instance.SetValue(currentHearts, maxHearts);
+
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Enable();
+        inputActions.Player.Movement.performed += OnMovement;
+        inputActions.Player.Movement.canceled += OnMovement;
+        inputActions.Player.Launch.performed += OnLaunch;
+        inputActions.Player.Shield.performed += OnShield;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -86,43 +93,37 @@ public class PlayerController : MonoBehaviour
                 isInvincible = false;
             }
         }
-
-        Vector2 move = new Vector2(horizontal, vertical);
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
-
-        animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (isAllowMagicShield == true)
-            {
-                StartCoroutine(CastSpell());
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            if (isAllowAttacking == true)
-            {
-                StartCoroutine(Launch());
-            }
-        }
     }
 
     void FixedUpdate()
     {
         if (isAllowMoving)
         {
-            Vector2 position = transform.position;
-            position.x += (2f * horizontal * Time.deltaTime * speed);
-            position.y += (2f * vertical * Time.deltaTime * speed);
-            rb2d.MovePosition(position);
+            if (currentInput.magnitude > 0.01)
+            {
+                Vector2 position = transform.position;
+                position.x += (2f * currentInput.x * speed * Time.deltaTime);
+                position.y += (2f * currentInput.y * speed * Time.deltaTime);
+                rb2d.MovePosition(position);
+            }
+
+            Vector2 move = new Vector2(currentInput.x, currentInput.y);
+            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+            {
+                //if (!footStepSource.isPlaying)
+                //    footStepSource.Play();
+                lookDirection.Set(move.x, move.y);
+                lookDirection.Normalize();
+            }
+            else
+            {
+                //footStepSource.Stop();
+            }
+
+
+            animator.SetFloat("Look X", lookDirection.x);
+            animator.SetFloat("Look Y", lookDirection.y);
+            animator.SetFloat("Speed", move.magnitude);
         }
     }
 
@@ -222,5 +223,39 @@ public class PlayerController : MonoBehaviour
         transform.position = position;
         yield return new WaitForSeconds(2f);
         isAllowTeleport = true;
+    }
+
+    // input actions
+    void OnMovement(InputAction.CallbackContext context)
+    {
+        Debug.Log(11);
+        if (context.performed)
+        {
+            currentInput = context.ReadValue<Vector2>();
+
+        }
+        if (context.canceled)
+        {
+            currentInput = Vector2.zero;
+        }
+    }
+
+    void OnLaunch(InputAction.CallbackContext context)
+    {
+
+        if (isAllowAttacking == true)
+        {
+            StartCoroutine(Launch());
+        }
+
+    }
+
+    void OnShield(InputAction.CallbackContext context)
+    {
+
+        if (isAllowMagicShield == true)
+        {
+            StartCoroutine(CastSpell());
+        }
     }
 }
